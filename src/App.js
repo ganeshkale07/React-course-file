2cdd304c21034ebfc18e95f025693e9a7ac0d94f
Ganesh Kale <ggkale07@gmail.com>
2021-03-10 15:50:33 +0530

    configuring react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import {Navbar , NavbarBrand} from 'reactstrap';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
