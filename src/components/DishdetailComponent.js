053666fc88f0dca05826a6ad244f53221fb76135
Ganesh Kale <ggkale07@gmail.com>
2021-03-22 17:32:05 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import {Control , LocalForm , Errors} from 'react-redux-form';
 import {Loading}  from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import {FadeTransform , Stagger , Fade} from 'react-animation-components';
 
 const required =(val) => val && val.length ; 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -118,20 +119,24 @@ class CommentForm extends Component {
         }
         const cmt = comments.map(comment => {
             return (
-                <List type="unstyled">
-                    <li key={comment.id}>
-                    <p>{comment.comment}</p>
-                    <p>-- {comment.author},
-                    &nbsp;
-                    {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
-                    </p>
-                    </li>
-                    
-                </List>
+                <Stagger in >
+                    <List type="unstyled">
+                        <Fade in>                        
+                            <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author},
+                            &nbsp;
+                            {new Intl.DateTimeFormat('en-US', {
+                                    year: 'numeric',
+                                    month: 'long',
+                                    day: '2-digit'
+                                }).format(new Date(comment.date))}
+                            </p>
+                            </li>
+                        </Fade>                       
+                    </List>
+                </Stagger>
+                                        
                 
                 
             )
@@ -150,6 +155,11 @@ class CommentForm extends Component {
          if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
+                    <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
                     <Card>
                         <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                         <CardBody>
@@ -157,6 +167,7 @@ class CommentForm extends Component {
                             <CardText>{dish.description}</CardText>
                         </CardBody>
                     </Card>
+                </FadeTransform>
                 </div>
             )
         }
